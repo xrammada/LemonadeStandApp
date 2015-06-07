e981d12b154888472b0849e4e0378821909096fd
Adam Marx <houdini.marx@gmail.com>
2015-06-07 07:14:56 -0400

    first update

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var MoneySupplyCount: UILabel!
+    @IBOutlet weak var LemonSupplyCount: UILabel!
+    @IBOutlet weak var IceCubeSupplyCount: UILabel!
+    @IBOutlet weak var lemonPurchaseCount: UILabel!
+    @IBOutlet weak var iceCubePurchaseCount: UILabel!
+    @IBOutlet weak var lemonMixCount: UILabel!
+    @IBOutlet weak var iceCubeMixCount: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //IBActions
 
+    @IBAction func lemonPurchasePlus(sender: UIButton) {
+    }
+    
+    @IBAction func lemonPurchaseMinus(sender: UIButton) {
+    }
+    
+    @IBAction func iceCubePurchasePlus(sender: UIButton) {
+    }
+    
+    @IBAction func iceCubePurchaseMinus(sender: UIButton) {
+    }
+    
+    @IBAction func lemonMixPlus(sender: UIButton) {
+    }
+    
+    @IBAction func lemonMixMinus(sender: UIButton) {
+    }
+    
+    @IBAction func iceCubeMixPlus(sender: UIButton) {
+    }
+    
+    @IBAction func iceCubeMixMinus(sender: UIButton) {
+    }
+    
+    @IBAction func startDay(sender: UIButton) {
+    }
+    
 }
 
